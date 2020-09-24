1a7b3e8ad33400cf2c888419f4b9f7f473c8741a
cmelgar <carlosaquijemelgar@gmail.com>
2020-09-24 17:00:09 -0500

    make it better

---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+        {
+            id: 'balut',
+            title: 'What is balut?',
+            content: 'If someone placed balut on your plate, you might, If someone placed balut on your plate, you might, If someone placed balut on your plate, you might'
+        },
+        {
+            id: 'whereIsIt',
+            title: 'Where is the sign in button?',
+            content: 'If someone placed balut on your plate, you might, If someone placed balut on your plate, you might, If someone placed balut on your plate, you might'
+        },
+        {
+            id: 'how',
+            title: 'How can I fix my problem?',
+            content: 'If someone placed balut on your plate, you might, If someone placed balut on your plate, you might, If someone placed balut on your plate, you might'
+        }
+    ]
+})
